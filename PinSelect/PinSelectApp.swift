65114035bcbb0109db92e1052ee49325d2ef8541
kelsey.makale <kelsey.makale@interswitchgroup.com>
2022-11-30 11:42:49 +0300

    Initial Commit

---
+++ PinSelect/PinSelectApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinSelectApp.swift
+//  PinSelect
+//
+//  Created by Kelsey Makale on 30/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct PinSelectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
